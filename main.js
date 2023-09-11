c4c045d71e4490356f158402bf8d1e878ed4d0ec
Debora Kuniy <debora.n_@hotmail.com>
2023-09-11 19:33:58 -0300

    otimizando as funcoes com for

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idAudio) {
+    document.querySelector(idAudio).play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador]
+    const somDaTecla = tecla.classList[1];    
+    const idAudio = `#som_${somDaTecla}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);                 
+    }
+}
 
 
