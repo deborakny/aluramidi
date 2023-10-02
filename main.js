dc06d4664369d1ffc0f9ef311bdfca90dbed6539
Debora Kuniy <debora.n_@hotmail.com>
2023-10-02 13:51:46 -0300

    adicionando a classe 'ativa' em um elemento

--- main.js
+++ main.js
@@ -12,6 +12,17 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     tecla.onclick = function () {
         tocaSom(idAudio);                 
     }
+
+    tecla.onkeydown = function (event) {
+        console.log(event.code)
+        if (event.code === "Space" || event.code === "Enter") {
+            tecla.classList.add('ativa');            
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
 
 
